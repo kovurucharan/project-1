2aa1ba5478b9c2c204523c5147f2bd868d3e7988
charan <kovurucharan@gmail.com>
2021-05-05 13:09:40 +0530

    initial commit

--- Food/urls.py
+++ Food/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("home/",include('WebApp.urls')),
+    path('',include('WebApp.urls')),
 ]
 
 if settings.DEBUG:
